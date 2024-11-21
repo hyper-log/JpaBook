c1b0aa131c2dcefa875015dd90af472decf9a825
hyper <revoluer9@gmail.com>
2024-11-21 18:42:50 +0900

    feat: 엔티티 클래스 개발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // JPA 스펙상 필요한 기본 생성자
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
