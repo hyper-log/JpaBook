aea5a40ef90d814b7a4dd828b16eb17974635ef5
hyper <revoluer9@gmail.com>
2024-12-05 19:47:26 +0900

    feat: 리팩토링

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -76,14 +76,14 @@ public class OrderRepository {
                 "select distinct o from Order o" +
                         " join fetch o.member m" +
                         " join fetch o.delivery d" +
-                        " join fetch  o.orderItems oi" +
+                        " join fetch o.orderItems oi" +
                         " join fetch oi.item i", Order.class)
                 .getResultList();
     }
 
     public List<Order> findAllWithMemberDelivery(int offset, int limit) {
         return em.createQuery(
-                        "select  o from  Order o" +
+                        "select o from Order o" +
                                 " join fetch o.member m" +
                                 " join fetch o.delivery d", Order.class)
                 .setFirstResult(offset)
